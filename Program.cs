43fbb7dcbb105b7d371ce1da91e9558dd22e4440
matt-culbert <culbertmatt@gmail.com>
2022-12-08 17:56:06 -0500

    rename

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-using System;
+﻿using System;
 using System.Net;
 using System.Net.Http;
 // https://qawithexperts.com/article/c-sharp/run-cmd-commands-using-c/520
 // https://stackoverflow.com/a/27326758/9329272
 
-namespace Test
+namespace Testing
 {
-    class Program
+    public class Program
     {
+        private static readonly HttpClient client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate });
         static void Main(string[] args)
         {
-			string command = "whoami";
+            string command = "whoami";
 			System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
 			procStartInfo.RedirectStandardOutput = true;
             procStartInfo.UseShellExecute = false;
@@ -24,15 +25,31 @@ namespace Test
             // Get the output into a string
             string whoami = proc.StandardOutput.ReadToEnd();
             Console.WriteLine("Making API Call...");
-            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
+            using (client)
             {
                 client.BaseAddress = new Uri("https://eoqqzdfuzmgq7gg.m.pipedream.net/");
-                HttpResponseMessage response = client.GetAsync(whoami).Result;
+                HttpResponseMessage response = client.GetAsync("").Result;
                 response.EnsureSuccessStatusCode();
                 string result = response.Content.ReadAsStringAsync().Result;
                 Console.WriteLine("Result: " + result);
             }
             Console.ReadLine();
+        }
+        public async void Post()
+        {
+            var values = new Dictionary<string, string>
+            {  
+                { "whoami", "test"}
+            };
+
+            //form "postable object" if that makes any sense
+            var content = new FormUrlEncodedContent(values);
+
+            //POST the object to the specified URI 
+            var response = await client.PostAsync("https://eoqqzdfuzmgq7gg.m.pipedream.net/", content);
+
+            //Read back the answer from server
+            var responseString = await response.Content.ReadAsStringAsync();
         }
     }
 }
