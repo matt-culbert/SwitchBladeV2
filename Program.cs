f8ff4b5e94cb8b0a8094e6eee7815fed9295acd9
matt-culbert <culbertmatt@gmail.com>
2022-12-19 23:00:33 -0500

    cleanup

--- Program.cs
+++ Program.cs
@@ -8,11 +8,49 @@ namespace Testing
 {
     public class Program
     {
-        static readonly HttpClient client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate });
+        static readonly HttpClient client = new HttpClient();
         static void Main()
         {
             string command = "whoami";
-			System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+			
+            string whoami = ExecuteMe(command);
+
+            Console.WriteLine(whoami);
+
+            Task<string> post = PostMe("https://eoqqzdfuzmgq7gg.m.pipedream.net/");
+
+            Console.WriteLine(post);
+
+            Task<string> stat = GetMe("https://eoqqzdfuzmgq7gg.m.pipedream.net/");
+
+            Console.WriteLine(stat);
+        }
+        static async Task<string> GetMe(string place, string subdir = "/"){
+                client.BaseAddress = new Uri(place);
+                HttpResponseMessage response = client.GetAsync(subdir).Result;
+                response.EnsureSuccessStatusCode();
+                string result = response.Content.ReadAsStringAsync().Result;
+                return result;
+        }
+        public static async Task<string> PostMe(string url)
+        {
+            var values = new Dictionary<string, string>
+            {  
+                { "accountidentifier", "Data you want to send at account field" },
+                { "type", "Data you want to send at type field"},
+                { "seriesid", "The data you went to send at seriesid field"
+                }
+            };
+            //form "postable object" if that makes any sense
+            var content = new FormUrlEncodedContent(values);
+            //POST the object to the specified URI 
+            var response = await client.PostAsync(url, content);
+            //Read back the answer from server
+            var responseString = await response.Content.ReadAsStringAsync();
+            return responseString;
+        }
+        static string ExecuteMe(string command){
+            System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
 			procStartInfo.RedirectStandardOutput = true;
             procStartInfo.UseShellExecute = false;
             // Do not create the black window.
@@ -23,33 +61,8 @@ namespace Testing
             proc.Start();
 
             // Get the output into a string
-            string whoami = proc.StandardOutput.ReadToEnd();
-            Console.WriteLine("Making API Call...");
-            using (client)
-            {
-                client.BaseAddress = new Uri("https://eoqqzdfuzmgq7gg.m.pipedream.net/");
-                HttpResponseMessage response = client.GetAsync("").Result;
-                response.EnsureSuccessStatusCode();
-                string result = response.Content.ReadAsStringAsync().Result;
-                Console.WriteLine("Result: " + result);
-            }
-            PostMe();
-        }
-        static async void PostMe()
-        {
-            var values = new Dictionary<string, string>
-            {  
-                { "whoami", "test"}
-            };
-
-            //form "postable object" if that makes any sense
-            var content = new FormUrlEncodedContent(values);
-
-            //POST the object to the specified URI 
-            var response = await client.PostAsync("https://eoqqzdfuzmgq7gg.m.pipedream.net/", content);
-
-            //Read back the answer from server
-            var responseString = await response.Content.ReadAsStringAsync();
+            string ressy = proc.StandardOutput.ReadToEnd();
+            return ressy;
         }
     }
 }
