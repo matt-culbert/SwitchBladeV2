b0d8cfb94d654f1a714d28eaf5bd90303075e529
matt-culbert <culbertmatt@gmail.com>
2022-12-20 21:36:01 -0500

    Update beacon.py

--- beacon.py
+++ beacon.py
@@ -28,7 +28,7 @@ hostname = hostname.strip()
 print(hostname)
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36 ',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1)',
     'APPSESSIONID': f'{GUID}',
     'RESPONSE': f'{hostname}'
 }
